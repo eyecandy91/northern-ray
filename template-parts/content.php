bddec26767d79de233450f746037496addfdd49c
Henry puttick <hputtick@me.com>
2019-11-15 12:45:03 +0700

    template match up for single blog posts/archives

--- template-parts/content.php
+++ template-parts/content.php
@@ -6,54 +6,29 @@
  *
  * @package _s
  */
-
+$image      = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
+$logo = get_theme_mod( 'logo_mobile' ); 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				_s_posted_on();
-				_s_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php _s_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', '_s' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+<div class="col-sm-12 col-md-4">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="card">
+            <a href="<?php the_permalink() ?>">
+                <div class="card-img-top blog-img" <?php 
+                    if ($image) : ?>
+                    style="background-image: url(<?php echo $image[0]; ?>); background-size: cover; background-position: right bottom"
+                    ; <?php else : ?>
+                    style="background-image: url(<?php echo $logo; ?>); background-position: center; background-size: 50%; background-color: #D10000; background-repeat: no-repeat;"
+                    <?php endif; ?>>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"><?php the_title(); ?></h5>
+                    <p class="card-text">
+                        <?php echo get_the_excerpt(); ?>
+                    </p>
+                </div>
+            </a>
+        </div>
+    </article><!-- #post-<?php the_ID(); ?> -->
+</div>
